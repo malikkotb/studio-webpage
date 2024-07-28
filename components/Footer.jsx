f82b819da026a4ed80158e69c373eeb6ff8dca67
malikkotb <malikkotb@icloud.com>
2024-07-28 13:13:50 +0200

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,21 @@
+"use client";
+import { motion } from "framer-motion";
 import React from "react";
 
 export default function Footer() {
   return (
     <div className="h-[65vh] border-red flex flex-col justify-between">
-      <div className="h-full leading-tight justify-center items-center flex flex-col">
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="h-full leading-tight justify-center items-center flex flex-col"
+      >
         <p className="text-[4vh] tracking-tight font-medium">Let's work together.</p>
         <p className="text-[4vh] text-[#7a7a7a] cursor-pointer hover:text-opacity-50 tracking-tight font-medium">
           Get in touch.
         </p>
-      </div>
+      </motion.div>
       <div className="flex justify-between p-8">
         <div className="flex gap-2">
           <div className="rounded-full p-3 bg-red-500"></div>
